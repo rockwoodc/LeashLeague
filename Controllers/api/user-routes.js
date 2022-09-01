2c02c63750755cd3548a3c643fa139bcfcf14e66
James-Fraser1 <jdfraser2022@gmail.com>
2022-09-01 10:53:49 -0400

    GET, POST, PUT, DELETE routes added for User.js

---
+++ Controllers/api/user-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET Users
+router.get('/', (req, res) => {
+
+});
+// GET Users by ID
+router.get('/:id', (req, res) => {
+
+});
+// POST Users
+router.post('/', (req, res) => {
+    
+});
+// PUT Users by ID
+router.put('/:id', (req, res) => {
+    
+});
+// DELTE Users by ID
+router.delete('/:id', (req, res) => {
+    
+});
+
+module.exports = router;
