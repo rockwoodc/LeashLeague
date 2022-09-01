9923b90187c5671b8be6ade68a26417d802fe077
James-Fraser1 <jdfraser2022@gmail.com>
2022-09-01 10:46:34 -0400

    User model created, new user-route created

---
+++ models/User.js
@@ -0,0 +1,63 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+const sequelize = require('sequelize');
+
+// Generates User Model
+class User extends Model {
+    // Compares passwords for each User's entry to make sure user entry reflects hashed result
+    comparePassword(loginPW) {
+        return bcrypt.compareSync(loginPW, this.password)
+    }
+}
+
+// Creating User structure
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            }
+        }
+    },
+    {
+        hooks: {
+            // Before Login Run new password through hash and saltRounds
+            async beforeCreate(newPW) {
+                newPW.password = await bcrypt.hash(newPW.password, saltRounds);
+                return newPW;
+            },
+            async beforeUpdate(encryptedPW) {
+                encryptedPW.password = await bcrypt.hash(encryptedPW.password, saltRounds);
+                return encryptedPW;
+            }
+        },
+        sequelize,
+        underscored: true,
+        timestamps: false,
+        modelName: 'user',
+        freezeTableName: true
+    }
+);
+
+module.exports = User;
