2bd98d995f6f027e8a00c2295ec009476fa74217
James-Fraser1 <jdfraser2022@gmail.com>
2022-09-02 16:28:43 -0400

    user index.js model file made

--- server.js
+++ server.js
@@ -38,41 +38,20 @@ sequelize.sync({ force: false}).then(() => {
     app.listen(PORT, () => console.log('Big Brother is now Listening!'))
 })
 
-// const fs = require('fs');
-// const path = require('path');
-// const Sequelize = require('sequelize');
-// const basename = path.basename(module.filename);
-// const env = process.env.NODE_ENV || 'development';
-// const config = require('./config/loginconnection')[env];
-// const db = {};
+// const express = require('express');
+// const routes = require('./Controllers');
+// const sequelize = require('./config/connection');
 
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable]);
-// } else {
-//   sequelize = new Sequelize(
-//     config.database, config.username, config.password, config
-//   );
-// }
+// const app = express();
+// const PORT = process.env.PORT || 3001;
 
-// fs
-//   .readdirSync(__dirname)
-//   .filter(file =>
-//     (file.indexOf('.') !== 0) &&
-//     (file !== basename) &&
-//     (file.slice(-3) === '.js'))
-//   .forEach(file => {
-//     const model = sequelize.import(path.join(__dirname, file));
-//     db[model.name] = model;
-//   });
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-// Object.keys(db).forEach(modelName => {
-//   if (db[modelName].associate) {
-//     db[modelName].associate(db);
-//   }
+// // turn on routes
+// app.use(routes);
+
+// // turn on connection to db and server
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
 // });
-
-// db.sequelize = sequelize;
-// db.Sequelize = Sequelize;
-
-// module.exports = db;
