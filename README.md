6226fc5093164675fad6dd36e9d37d71601e2405
rockwoodc <rockwoodce@gmail.com>
2022-09-07 20:23:34 -0400

    readme added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# LeashLeague
+# Leash League
+
+## By The PUPaholics-- James Fraser, Cortland Finch, Christopher Campaigne, and Christian Rockwood
+
+As devoted dog owners and lovers we wanted to make it simple for you to find your new fury bestfriend in a shelter near you. As we all know dogs can come in all shapes and sizes with different tempermeants and physical activity needs. This often makes it difficult for new potential dog owners to choose which breed they want or can have depending on their situation at home.We have created what we beleive is a great starting point in choosing your new companion. Our hope is to reduce or eliminate "shopping" for dogs and empty out our local shelters!
+
+![Generator](./imgs/ScreenShot.png)
+
+[Link to Deployed Application]()
--- public/javascript/api.js
+++ public/javascript/api.js
@@ -1,15 +1,23 @@
-//URL- https://api.thedogapi.com/v1/images/search
-var apiKey = "live_vqm057yKxYgjVnFyPLRJ1YAljtY8n1Qc6c4oUAF9Uv8UVnUTC4qKi1noDSljZ2No"
+// var apiURL = "https://api.thedogapi.com/v1"
+// //breed endpoint
+// var breedEndpoint = '/breeds'
+
+//&api_key= added to the key
+var apiKey = "&api_key=live_vqm057yKxYgjVnFyPLRJ1YAljtY8n1Qc6c4oUAF9Uv8UVnUTC4qKi1noDSljZ2No"
 
 
 //use api to get breed information
-const getDogBreeds = () => {
-    const data = fetch('https://api.thedogapi.com/v1/breeds')
+const getDogBreeds = (dogbreed) => {
+    const data = fetch(`https://api.thedogapi.com/v1/breed/${dogbreed}`)
     //return the data
     .then(data => data.json())
+    //display data
+
     //if an error occurs
     .catch(error => console.error('Could not fetch your data'));
     
 }
 
-getDogBreeds();
+getDogBreeds("bulldog");
+
+//display api info on screen
